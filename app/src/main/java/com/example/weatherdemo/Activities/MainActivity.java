df9922c0aded08cb5c66823147d4df03b608051a
Andy Lemz <Andy.Lemz@gmail.com>
2020-04-19 21:55:36 +0300

    Косметически отредактирвоан основной макет, добавлен горизонтальный вид, переписан класс CitiesActivity, экран выбора города установлен первым экраном, реализована передача города в основной класс

--- app/src/main/java/com/example/weatherdemo/Activities/MainActivity.java
+++ app/src/main/java/com/example/weatherdemo/Activities/MainActivity.java
@@ -1,18 +1,39 @@
 package com.example.weatherdemo.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.weatherdemo.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String CityLocalizedName = "";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        setContentView(R.layout.choose_city);
+        setContentView(R.layout.activity_main);
         //setContentView(R.layout.settings);
+
+        CityLocalizedName = getIntent().getStringExtra("CityLocalizedName");
+        TextView CurrentCity = (TextView) findViewById(R.id.CurrentCity);
+        CurrentCity.setText(CityLocalizedName);
+
+        SetTemperature();
+    }
+
+    public void SetTemperature(){
+        TextView CurrentTemperatureForm = (TextView) findViewById(R.id.CurrentTemperature);
+        TextView Plus1TForm = (TextView) findViewById(R.id.Plus1T);
+        TextView Plus2TForm = (TextView) findViewById(R.id.Plus2T);
+        TextView Plus3TForm = (TextView) findViewById(R.id.Plus3T);
+
+        CurrentTemperatureForm.setText("15");
+        Plus1TForm.setText("17");
+        Plus2TForm.setText("16");
+        Plus3TForm.setText("17");
     }
 
 }
---
+++ app/src/main/java/com/example/weatherdemo/Weather.java
@@ -0,0 +1,14 @@
+package com.example.weatherdemo;
+
+public class Weather {
+    public String CityID;
+
+    public int[] GetWeatherByCityID(String CityID){
+
+        // ручная заглушка
+        int[] arr = {17, 16, 15, 14};
+
+        return arr;
+    }
+
+}
